a53e5fba0fe137eb95875b050ad63bc29b5a91e9
samiullah997 <samiullahk997@gmail.com>
2022-11-08 17:51:13 +0500

    implement save_books method to store book in file and also save_rentals method to store rentals record in file

--- save_data.rb
+++ save_data.rb
@@ -21,4 +21,48 @@ class SaveData
       File.write('./data/people.json', JSON.pretty_generate(people_array))
     end
   end
+
+  def save_books(books)
+    books_array = []
+    books.each do |book|
+      books_array << {title: book.title, author: book.author}
+    end
+    p books_array
+
+    if !books_array.empty? 
+      check_file_exists('books')
+      File.write('./data/books.json', JSON.pretty_generate(books_array))
+    end
+  end
+
+  def save_rentals(rentals)
+    rentals_array = []
+    rentals.each do |rental|
+      rental_obj = {
+        date: rental.date,
+        title: rental.book.title,
+        author: rental.book.author,
+        id: rental.person.id,
+        name: rental.person.name,
+        age: rental.person.age,
+        parent_permission: rental.person.parent_permission,
+        type: rental.person.type
+      }
+      if rental.person.type == 'Student'
+        rental_obj[:classroom] = rental.person.classroom
+      else
+        rental_obj[:specialization] = rental.person.specialization
+      end
+      rentals_array << rental_obj
+    end
+    p rentals_array
+
+    if !rentals_array.empty? 
+      check_file_exists('rentals')
+      if !File.exists?('./data/rentals.json')
+        FileUtils.touch('./data/rentals.json')
+      end
+      File.write('./data/rentals.json', JSON.pretty_generate(rentals_array))
+    end
+  end
 end

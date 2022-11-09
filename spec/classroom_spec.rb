773731e9b3f793a6631a7fbe5f6b07741a044f90
michael-duke <michael.21duke@gmail.com>
2022-11-09 17:03:12 +0300

    Add addtional test for classroom and fix Rubocop errors

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -10,7 +10,7 @@ describe Classroom do
     it 'should have a label property' do
       expect(@classroom.label).to eql 'CS50'
     end
-    it 'the label should be an integer, a numeric value' do
+    it 'the label should be a string' do
       expect(@classroom.label).to be_kind_of String
     end
     it 'should be an instance of the Classroom class' do
@@ -18,11 +18,14 @@ describe Classroom do
     end
   end
   context 'When adding classroom for the Student' do
+    it 'can add a student' do
+      expect(@classroom).to respond_to(:add_student)
+    end
     it 'Can add new Student for the Classroom' do
       @classroom.add_student(@student)
       expect(@classroom.students.count).to eq(1)
     end
-    it 'Should assign classroom to student' do
+    it 'Should assign classroom to student upon adding the student to the classroom' do
       expect(@student.classroom).to eql 'B'
       @classroom.add_student(@student)
       expect(@student.classroom.label).to eql 'CS50'
